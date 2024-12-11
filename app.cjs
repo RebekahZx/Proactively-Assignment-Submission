f9511e8f2bb351e5f7b439d0b172dd72e07d5620
Shrinivas anavekar <129083888+RebekahZx@users.noreply.github.com>
2024-12-11 22:57:17 +0530

    app.cjs

--- app.cjs
+++ app.cjs
@@ -1,18 +1,4 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const authRoutes = require('./routes/authRoutes');
 
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use('/auth', authRoutes);
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const express = require('express');
 const bodyParser = require('body-parser');
